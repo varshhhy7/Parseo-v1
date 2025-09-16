1b41c53f774934c6e1a363beb5c28c8660f62755
Varshith <manivarshithpc@gmail.com>
2025-09-16 20:49:18 +0530

    passed spaces

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,5 +1,7 @@
+import shutil
+import time
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app,BASE_DIR
 
 client = TestClient(app)
 
@@ -14,3 +16,15 @@ def test_post_home():
     assert response.status_code == 200
     assert "application/json" in response.headers["content-type"]
     assert response.json() == {"hello": "world"}
+
+
+def test_echo_upload():
+    image_saved_path=BASE_DIR /"images"
+
+    for path in image_saved_path.glob("*"):
+        response = client.post("/img-echo/",files={"file": open(path,'rb')})
+        assert response.status_code == 200
+        print(response.headers)
+        ##assert "application/json" in response.headers["content-type"]
+        ##assert response.json()=={"hello":"world"}
+
